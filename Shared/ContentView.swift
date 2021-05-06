02e5763f560bee9f725098cb740ff1928bb3cd6f
Centauria <centauriavc@gmail.com>
2021-05-06 15:38:13 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Centauria V. CHEN on 2021/5/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Vocal_TunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vocal_TunerApp.swift
+//  Shared
+//
+//  Created by Centauria V. CHEN on 2021/5/6.
+//
+
+import SwiftUI
+
+@main
+struct Vocal_TunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
